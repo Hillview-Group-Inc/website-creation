ae9468d1f9a4e7b913766cfdbe49b19da5ff5496
Olugbenga Oguntokun <ooguntokun@hillviewgroupinc.com>
2026-03-09 10:49:43 -0400

    included Access control allow origin header in CORS middleware

--- hillview-functions/src/functions/contact.js
+++ hillview-functions/src/functions/contact.js
@@ -16,6 +16,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
 };
 app.use(cors(corsOptions));
 
