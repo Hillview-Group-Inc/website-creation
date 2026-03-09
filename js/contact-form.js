669139f27b344704b204282e4e51cf649b4cbfce
Olugbenga Oguntokun <ooguntokun@hillviewgroupinc.com>
2026-03-08 21:34:23 -0400

    changed endpoint URL in contact-form.js

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_BASE_URL = "http://localhost:3000"; // Change this to your production URL when deploying
+const API_BASE_URL = "https://orange-sea-03d42eb0f.2.azurestaticapps.net"; //"http://localhost:3000"; // Development URL when local testing
 
 // Contact Form Handling
 document
